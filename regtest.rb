f38552e7de26a5cb6ec93aa7063d9c926c67bfe4
Simon Ordish <simon.ordish@masagi.co.uk>
2025-03-20 17:03:31 +0100

    Correct tar name

--- regtest.rb
+++ regtest.rb
@@ -1,7 +1,7 @@
 class Regtest < Formula
   desc "Small utility to run bitcoin-sv regtest on a mac"
   homepage "https://github.com/ordishs/bitcoin-sv-regtest"
-  url "https://github.com/ordishs/bitcoin-sv-regtest/releases/1.0.0.tar.gz"
+  url "https://github.com/ordishs/bitcoin-sv-regtest/releases/v1.0.0.tar.gz"
   sha256 "386fcdb8ee1e59d4eb79e80178b540e9d288fd3b3e073c21a4fd074efe4d40d4"
   version "1.0.0"
 
