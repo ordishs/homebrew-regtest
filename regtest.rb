ebd05f8fc83761f1d118e06f50b98a79ab43bf71
Simon Ordish <simon.ordish@masagi.co.uk>
2025-03-20 19:25:47 +0100

    fix checksum

--- regtest.rb
+++ regtest.rb
@@ -2,7 +2,7 @@ class Regtest < Formula
   desc "Small utility to run bitcoin-sv regtest on a mac"
   homepage "https://github.com/ordishs/bitcoin-sv-regtest"
   url "https://github.com/ordishs/bitcoin-sv-regtest/releases/download/v1.0.2/bitcoin-sv-regtest-1.0.2.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b51"
+  sha256 "2ae76c6ea5ed274698e2dc9f15b1db9606582b361ada31f6e6134d748e14c204"
   version "1.0.2"
 
   def install
