1d4243d33d802467c8e338b3855d0c63e8039fb8
Simon Ordish <simon.ordish@masagi.co.uk>
2025-03-21 01:18:04 +0100

    v1.0.3

--- regtest.rb
+++ regtest.rb
@@ -1,9 +1,9 @@
 class Regtest < Formula
   desc "Small utility to run bitcoin-sv regtest on a mac"
   homepage "https://github.com/ordishs/bitcoin-sv-regtest"
-  url "https://github.com/ordishs/bitcoin-sv-regtest/releases/download/v1.0.2/bitcoin-sv-regtest-1.0.2.tar.gz"
-  sha256 "2ae76c6ea5ed274698e2dc9f15b1db9606582b361ada31f6e6134d748e14c204"
-  version "1.0.2"
+  url "https://github.com/ordishs/bitcoin-sv-regtest/releases/download/v1.0.  3/bitcoin-sv-regtest-1.0.3.tar.gz"
+  sha256 "c98e40274830b9613d0e6cc8f4822e79935cfcc9b3becb042b5df6b76b0e0192"
+  version "1.0.3"
 
   def install
     bin.install "regtest"
